a75255cd93156c61e806c37d5867efbdd12f00b7
JesusIasenzaniro <81029273+JesusIasenzaniro@users.noreply.github.com>
2024-07-12 13:36:50 +0200

    update app and index to use context and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
-function App() {
-    return <div className='App'>hello</div>;
-}
+import React from 'react';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import CharacterListPage from './pages/CharacterListPage/CharacterListPage';
+import CharacterDetailPage from './pages/CharacterDetailPage/CharacterDetailPage';
+import FavoritesPage from './pages/FavoritesPage/FavoritesPage';
+import Header from './components/organisms/ORHeader/Header';
+import { AnimatePresence } from 'framer-motion';
+
+const App: React.FC = () => {
+    const location = useLocation();
+    return (
+        <>
+            <Header />
+            <AnimatePresence mode='wait'>
+                <Routes location={location}>
+                    <Route path='/' element={<CharacterListPage />} />
+                    <Route
+                        path='/character/:id'
+                        element={<CharacterDetailPage />}
+                    />
+                    <Route path='/favorites' element={<FavoritesPage />} />
+                </Routes>
+            </AnimatePresence>
+        </>
+    );
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { FavoritesProvider } from './contexts/FavoritesContext';
+import { SearchProvider } from './contexts/SearchContext';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <FavoritesProvider>
+            <SearchProvider>
+                <Router>
+                    <Routes>
+                        <Route path='/*' element={<App />} />
+                    </Routes>
+                </Router>
+            </SearchProvider>
+        </FavoritesProvider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
