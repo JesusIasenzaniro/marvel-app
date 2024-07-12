73b61b34c16a318df3eefab71695d81cc93aedc3
JesusIasenzaniro <81029273+JesusIasenzaniro@users.noreply.github.com>
2024-07-12 13:31:11 +0200

    create search bar component

---
+++ src/components/molecules/MLSearchBar/SearchBar.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { FaSearch } from 'react-icons/fa';
+import { useSearch } from '../../../contexts/SearchContext';
+import './SearchBar.css';
+
+const SearchBar: React.FC = () => {
+    const { query, setQuery } = useSearch();
+
+    return (
+        <form className='search-bar' onSubmit={(e) => e.preventDefault()}>
+            <FaSearch />
+            <input
+                type='text'
+                placeholder='SEARCH CHARACTER...'
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+            />
+        </form>
+    );
+};
+
+export default SearchBar;
