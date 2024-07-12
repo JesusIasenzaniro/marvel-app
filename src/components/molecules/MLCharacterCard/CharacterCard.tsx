be42b2f3c6a7080bec5e2823b54efa7f4cd39a39
JesusIasenzaniro <81029273+JesusIasenzaniro@users.noreply.github.com>
2024-07-12 13:32:57 +0200

    create character card component

---
+++ src/components/molecules/MLCharacterCard/CharacterCard.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useFavorites } from '../../../contexts/FavoritesContext';
+import './CharacterCard.css';
+import FavoriteButton from '../MLFavoriteButton/FavoriteButton';
+
+interface CharacterCardProps {
+    character: {
+        id: number;
+        name: string;
+        thumbnail: {
+            path: string;
+            extension: string;
+        };
+    };
+    isDetailPage?: boolean;
+}
+
+const CharacterCard: React.FC<CharacterCardProps> = ({
+    character,
+    isDetailPage = false,
+}) => {
+    const navigate = useNavigate();
+    const { toggleFavorite, favorites } = useFavorites();
+    const isFavorite = favorites.includes(character.id);
+
+    return (
+        <div className='character-card'>
+            <img
+                src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
+                alt={character.name}
+                onClick={() => navigate(`/character/${character.id}`)}
+            />
+            <div className='separator'></div>
+            <div className='card-content'>
+                <h3>{character.name}</h3>
+                <FavoriteButton
+                    isFavorite={isFavorite}
+                    toggleFavorite={() => toggleFavorite(Number(character.id))}
+                    isDetailPage={isDetailPage}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default CharacterCard;
